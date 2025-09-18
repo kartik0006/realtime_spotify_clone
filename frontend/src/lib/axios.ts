419650f17e731e8067bb208bef0af00ca7902557
Kartik <kartik@Kartiks-Macbook-M3.local>
2025-09-19 00:07:36 +0530

    Fix axios config and vercel setup

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-	baseURL: import.meta.env.MODE === "development" ? "http://localhost:5003/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5003/api"
+      : "https://realtime-spotify-clone-4a90.onrender.com/api",
+  withCredentials: true,
 });
